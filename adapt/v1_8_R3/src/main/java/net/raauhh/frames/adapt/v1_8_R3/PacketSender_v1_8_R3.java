54122db382305e046b74968927c416d68728b27b
Raauhh <raul4canena@gmail.com>
2022-05-30 13:25:02 +0200

    feat(v1_8_R3): updated spigot dependency.

--- adapt/v1_8_R3/src/main/java/net/raauhh/frames/adapt/v1_8_R3/PacketSender_v1_8_R3.java
+++ adapt/v1_8_R3/src/main/java/net/raauhh/frames/adapt/v1_8_R3/PacketSender_v1_8_R3.java
@@ -1,6 +1,5 @@
 package net.raauhh.frames.adapt.v1_8_R3;
 
-import com.google.inject.Singleton;
 import net.minecraft.server.v1_8_R3.Packet;
 import net.minecraft.server.v1_8_R3.PlayerConnection;
 import net.raauhh.frames.api.util.PacketSender;
@@ -8,7 +7,6 @@ import org.apache.commons.lang3.Validate;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-@Singleton
 public class PacketSender_v1_8_R3 implements PacketSender {
 
   @Override
